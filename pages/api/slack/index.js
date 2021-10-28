6776f653a0e0e6ba3453ba6d527d51294ed9d8e2
Vinícius Oliveira <eu@viniciusoliveira.me>
2021-10-28 10:45:03 -0300

    Added gif to slack api

--- pages/api/slack/index.js
+++ pages/api/slack/index.js
@@ -10,19 +10,46 @@ import Time from '../../../helpers/time'
 export default (req, res) => {
   let timezone = req.body.text || req.query.tz || Time.DEFAULT_TIMEZONE
   let time = Time.validOrNull(timezone)
+  const chosenReason = getRandom(dayHelper(time))
+  const reasonText = chosenReason.reason
+  const gifUrl = getRandom(chosenReason.gifs)
+
 
   res.status(200).json({
     response_type: time ? 'in_channel' : 'ephemeral',
-    attachments: [
+    blocks: [
       {
-        text: time
-          ? getRandom(dayHelper(time)).reason
-          : `Invalid time zone: '${timezone}'`,
-        color: shouldIDeployColorTheme(time),
-        thumb_url: shouldIDeployAnswerImage(time),
-        footer_icon: shouldIDeployFavIcon(time),
-        footer: 'Should I deploy today' + (time ? ` | ${timezone}` : '')
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*"+time
+          ? reasonText
+          : `Invalid time zone: '${timezone}'`+"*"
+        },
+        accessory: {
+          type: "image",
+          image_url: shouldIDeployAnswerImage(time)
+        }
+      },
+      {
+        type: "image",
+        image_url: gifUrl,
       }
     ]
   })
+
+  // res.status(200).json({
+  //   response_type: time ? 'in_channel' : 'ephemeral',
+  //   attachments: [
+  //     {
+  //       text: time
+  //         ? getRandom(dayHelper(time)).reason
+  //         : `Invalid time zone: '${timezone}'`,
+  //       color: shouldIDeployColorTheme(time),
+  //       thumb_url: shouldIDeployAnswerImage(time),
+  //       footer_icon: shouldIDeployFavIcon(time),
+  //       footer: 'Should I deploy today' + (time ? ` | ${timezone}` : '')
+  //     }
+  //   ]
+  // })
 }
